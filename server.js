063b4a6c14bb4ccba5793d9fcc98c3c23021311e
OpkWan <alexopoku83@gmail.com>
2024-07-27 19:34:05 +0000

    added port and Mongo Connections

--- server.js
+++ server.js
@@ -15,3 +15,7 @@ MongoClient.connect(dbConnectionString)
         db = client.db(dbName)
         collection = db.collection("aircrafts")
     })
+
+app.listen(process.env.PORT || PORT, () =>{
+    console.log(`Server is running on port ${process.env.PORT}`);
+})
